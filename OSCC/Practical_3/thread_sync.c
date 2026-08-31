7ea873f2665a668f6a1edd373888bc167a02a679
Varun Jagtap <varunjagtap424@gmail.com>
2026-08-31 16:16:51 +0530

    Added OSCC 3rd practical

---
+++ OSCC/Practical_3/thread_sync.c
@@ -0,0 +1,58 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int counter = 0;
+
+/* Declare Mutex */
+pthread_mutex_t mutex;
+
+/* Function executed by threads */
+void *incrementCounter(void *arg) {
+  int thread_no = *((int *)arg);
+
+  for (int i = 0; i < 5; i++) {
+    /* Lock the critical section */
+    pthread_mutex_lock(&mutex);
+
+    counter++;
+
+    printf("Thread %d -> Counter = %d\n", thread_no, counter);
+
+    /* Unlock the critical section */
+    pthread_mutex_unlock(&mutex);
+  }
+
+  pthread_exit(NULL);
+}
+
+int main() {
+  pthread_t thread1, thread2;
+
+  int id1 = 1;
+  int id2 = 2;
+
+  /* Initialize mutex */
+  pthread_mutex_init(&mutex, NULL);
+
+  printf("Main thread started...\n");
+
+  /* Create first thread */
+  pthread_create(&thread1, NULL, incrementCounter, &id1);
+
+  /* Create second thread */
+  pthread_create(&thread2, NULL, incrementCounter, &id2);
+
+  /* Wait for threads to finish */
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+
+  printf("\nFinal Counter Value = %d\n", counter);
+
+  /* Destroy mutex */
+  pthread_mutex_destroy(&mutex);
+
+  printf("Main thread terminated.\n");
+
+  return 0;
+}
